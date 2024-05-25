090125dcef70a3d2558c82c55c2568a53ed8b45e
Abdishakur adan <abdishakuradan4040@gmail.com>
2024-05-25 02:51:02 -0700

    exit complete and theme and also time

--- gui.py
+++ gui.py
@@ -1,24 +1,30 @@
 import function
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkPurple4")
 
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a To-do")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values=function.opening_files(),key='todos',
+list_box = sg.Listbox(values=function.opening_files(), key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button =sg.Button("Exit")
 
-window = sg.Window('MY To-Do App', layout=[[label],[input_box, add_button],[list_box,edit_button]],
-                   font=('Helvetica',20))
+window = sg.Window('MY To-Do App', layout=[[clock],[label], [input_box, add_button], 
+                                           [list_box, edit_button, complete_button],
+                                           [exit_button]],
+                                           font=('Helvetica',20))
 
 
 
 while True:
-    event,values = window.read()
-    print(1,event)
-    print(2,values)
-    print(3,values['todos'])
+    event,values = window.read(timeout=200)
+    window["clock"].update(value =time.strftime("%b %d, %Y %H:%M:%S"))
+  
     match event:
         case "Add":
             todos = function.opening_files()
@@ -28,17 +34,31 @@ while True:
             window['todos'].update(values = todos)
 
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+                try:
+                    todo_to_edit = values['todos'][0]
+                    new_todo = values['todo']
 
-            todos = function.opening_files()
-            index = todos.index(todo_to_edit)
-            todos[index]=new_todo
-            function.write_todos(todos)
-
-            window['todos'].update(values=todos)
+                    todos = function.opening_files()
+                    index = todos.index(todo_to_edit)
+                    todos[index]=new_todo
+                    function.write_todos(todos)
+                    window['todos'].update(values=todos)
+                except IndexError:
+                    sg.popup("Please select an item First.", font=("Helvetica", 20))    
+        case "Complete":
+             try:
+                todo_to_complete = values['todos'][0]
+                todos = function.opening_files()
+                todos.remove(todo_to_complete)
+                function.write_todos(todos)
+                window['todos'].update(values = todos)
+                window['todo'].update(value ='')
+             except IndexError:
+                 sg.popup("Please select an item First.", font=("Helvetica", 20))    
+        case "Exit":
+            break
         case 'todos':
-            window['todo'].update(value = values['todos'][0])
+            window['todo'].update(value = values['todos'][0])    
         case sg.WIN_CLOSED:
             break    
 
